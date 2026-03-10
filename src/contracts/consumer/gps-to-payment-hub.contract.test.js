e229a4bfab62d0baf3aa58d0548e0ff411f46c09
SeedSyntax <221134143+SeedSyntax@users.noreply.github.com>
2026-03-10 12:22:20 +0000

    SFIR-1028 Use Pact to validate data being sent to Payments Hub

--- src/contracts/consumer/gps-to-payment-hub.contract.test.js
+++ src/contracts/consumer/gps-to-payment-hub.contract.test.js
@@ -2,7 +2,7 @@ import { vi } from 'vitest'
 
 import crypto from 'node:crypto'
 
-import { Pact } from '@pact-foundation/pact'
+import { Pact, MatchersV3 } from '@pact-foundation/pact'
 
 import { createServer } from '#~/server.js'
 import { config } from '#~/config/index.js'
@@ -23,7 +23,7 @@ describe('cron job schedule sending a POST request to payment hub', () => {
   const provider = new Pact({
     consumer: 'grants-payment-service',
     provider: 'payment-hub',
-    logLevel: process.env.CI ? 'warn' : 'info',
+    logLevel: process.env.CI ? 'info' : 'debug',
     ...withPactDir(import.meta.url)
   })
 
@@ -63,10 +63,48 @@ describe('cron job schedule sending a POST request to payment hub', () => {
   it('should send a request to payment hub to setup a new payment schedule', async () => {
     return await provider
       .addInteraction()
-      .given('A customer has an agreement offer')
-      .uponReceiving('a request from the customer to accept their offer')
+      .given('A customer has an active SFI grant')
+      .uponReceiving('the due date for the grant payment has been reached')
       .withRequest('POST', '/messages', (builder) => {
-        builder.headers({ 'Content-Type': 'application/json' })
+        builder.headers({
+          'Content-Type': 'application/json',
+          Authorization: MatchersV3.regex(
+            /^SharedAccessSignature sr=http%3A%2F%2F127.0.0.1%3A\d+&sig=.*&se=.*&skn=test-key-name$/,
+            'SharedAccessSignature sr=http%3A%2F%2F127.0.0.1%3A63002&sig=O64%2F7369ZY3Qh3kGCFiFkaYBaFvwJyFCnpdjQdaAZFY%3D&se=1780711740&skn=test-key-name'
+          ),
+          brokerproperties: MatchersV3.regex(
+            /\{"SessionId":".*"\}/,
+            '{"SessionId":"123"}'
+          )
+        })
+        builder.jsonBody({
+          sourceSystem: 'FPTT',
+          ledger: 'AP',
+          deliveryBody: 'RP00',
+          invoiceNumber: 'R00000004-V001Q2',
+          fesCode: 'FALS_FPTT',
+          marketingYear: '2026',
+          paymentRequestNumber: 1,
+          agreementNumber: 'FPTT264870631',
+          currency: 'GBP',
+          dueDate: '05/06/2026',
+          remittanceDescription: 'Farm Payments Technical Test Payment',
+          invoiceLines: [
+            {
+              schemeCode: 'CMOR1',
+              accountCode: 'SOS710',
+              fundCode: 'DRD10',
+              agreementNumber: 'FPTT264870631',
+              description:
+                'Parcel 8083 - Assess moorland and produce a written record',
+              value: '12.63',
+              deliveryBody: 'RP00',
+              marketingYear: '2026'
+            }
+          ],
+          correlationId: '7cf9bd11-c791-42c9-bd28-fa0fec_id',
+          value: '702.85'
+        })
       })
       .willRespondWith(200, (builder) => {
         builder.body('text/plain; charset=utf-8', Buffer.from(''))
