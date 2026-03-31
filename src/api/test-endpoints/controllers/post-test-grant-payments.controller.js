1ee1741a388358663bcc56444dda68684cc581ee
Asif Shaik <45974468+asifshaik04@users.noreply.github.com>
2026-03-31 11:48:12 +0100

    SFIR-1070: removed overlap check validation (#49)

--- src/api/test-endpoints/controllers/post-test-grant-payments.controller.js
+++ src/api/test-endpoints/controllers/post-test-grant-payments.controller.js
@@ -1,7 +1,7 @@
 import { statusCodes } from '#~/common/constants/status-codes.js'
 import { createGrantPayment } from '#~/common/helpers/create-grant-payment.js'
-import { fetchGrantPaymentsBySbi } from '#~/common/helpers/fetch-grant-payments-by-sbi.js'
 import { serializeError } from '#~/common/helpers/serialize-error.js'
+import { prepareWithPaymentHubConfig } from '#~/common/helpers/payment-hub/prepare-with-payment-hub-config.js'
 
 const postTestGrantPaymentController = {
   method: 'POST',
@@ -18,18 +18,11 @@ const postTestGrantPaymentController = {
   handler: async (req, res) => {
     try {
       const payload = req.payload
-      const { sbi } = payload
 
-      if (await overlappingDatesInGrantPayments(sbi, payload)) {
-        return res
-          .response({
-            message: 'Validation error',
-            error: 'For the given sbi overlapping grant payment already exists'
-          })
-          .code(statusCodes.badRequest)
-      }
+      const grantPaymentWithPaymentHubConfig =
+        prepareWithPaymentHubConfig(payload)
 
-      const created = await createGrantPayment(payload)
+      const created = await createGrantPayment(grantPaymentWithPaymentHubConfig)
 
       const id = created?._id?.toString?.() || created?.id || created?._id
 
@@ -57,61 +50,6 @@ const postTestGrantPaymentController = {
         .code(statusCodes.internalServerError)
     }
   }
-}
-
-const fetchAllDueDates = (incomingPayload) => {
-  const incomingDueDates = new Set()
-  incomingPayload.grants?.forEach((grant) => {
-    if (grant.dueDate) {
-      incomingDueDates.add(grant.dueDate)
-    }
-    grant.payments?.forEach((payment) => {
-      if (payment.dueDate) {
-        incomingDueDates.add(payment.dueDate)
-      }
-    })
-  })
-  return incomingDueDates
-}
-
-const checkExistingGrantsMatchingDueDates = (
-  existingGrants,
-  incomingDueDates
-) => {
-  return existingGrants.some((grant) => {
-    if (grant.dueDate && incomingDueDates.has(grant.dueDate)) {
-      return true
-    }
-
-    return (grant.payments || []).some(
-      (payment) => payment.dueDate && incomingDueDates.has(payment.dueDate)
-    )
-  })
-}
-
-const dueDatesComparisionCheckWithExistingGrantPaymentsDates = async (
-  incomingDueDates,
-  existingRecordsMatchingSbi
-) => {
-  return existingRecordsMatchingSbi.some((record) => {
-    const grants = record.grants || []
-    return checkExistingGrantsMatchingDueDates(grants, incomingDueDates)
-  })
-}
-
-const overlappingDatesInGrantPayments = async (sbi, incomingPayload) => {
-  const existingGrantsMatchingSbi = await fetchGrantPaymentsBySbi(sbi)
-
-  if (!existingGrantsMatchingSbi || existingGrantsMatchingSbi.length === 0) {
-    return false
-  }
-
-  const incomingDueDates = fetchAllDueDates(incomingPayload)
-
-  return dueDatesComparisionCheckWithExistingGrantPaymentsDates(
-    incomingDueDates,
-    existingGrantsMatchingSbi
-  )
 }
 
 export { postTestGrantPaymentController }
