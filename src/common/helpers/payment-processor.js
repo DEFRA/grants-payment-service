bf430964ffe9d4a15a29ca59dabbddf1bd1fe620
SeedSyntax <221134143+SeedSyntax@users.noreply.github.com>
2026-04-07 14:29:45 +0100

    SFIR-1223 Get complete grant payments to compute correct quarter offset

--- src/common/helpers/payment-processor.js
+++ src/common/helpers/payment-processor.js
@@ -5,6 +5,7 @@ import { updatePaymentStatus } from '#~/common/helpers/update-payment-status.js'
 import { transformFpttPaymentDataToPaymentHubFormat } from '#~/common/helpers/payment-hub/fptt-data-transformer.js'
 import { serializeError } from '#~/common/helpers/serialize-error.js'
 import { grafanaLogMessages } from '#~/common/constants/grafana-log-messages.js'
+import GrantPaymentsModel from '#~/api/common/models/grant_payments.js'
 
 export const processDailyPayments = async (server, date = getTodaysDate()) => {
   const { logger } = server
@@ -18,65 +19,76 @@ export const processDailyPayments = async (server, date = getTodaysDate()) => {
     )
 
     // flatten document/grant/payment hierarchy into a simple list of payment items
-    const actions = (docs || []).flatMap((doc) => {
+    const actions = []
+    for (const doc of docs || []) {
       const { _id: docId, sbi, frn, claimId, grants } = doc
 
-      return (grants || []).flatMap((grant) => {
-        const { payments } = grant
-
-        return (payments || []).map(async (payment) => {
-          // attempt to lock the payment; only succeed if it is still pending.
-          const lockResult = await updatePaymentStatus(
-            docId,
-            payment._id,
-            'locked',
-            'pending'
+      for (const grantWithPendingPayments of grants || []) {
+        const { payments } = grantWithPendingPayments
+        const grant = (
+          await GrantPaymentsModel.findOne(
+            { _id: docId, 'grants._id': grantWithPendingPayments._id },
+            { 'grants.$': 1 }
           )
+        ).grants[0]
 
-          const wasLocked =
-            lockResult &&
-            (lockResult.nModified === 1 ||
-              lockResult.modifiedCount === 1 ||
-              lockResult.n === 1)
+        for (const payment of payments || []) {
+          actions.push(
+            (async () => {
+              // attempt to lock the payment; only succeed if it is still pending.
+              const lockResult = await updatePaymentStatus(
+                docId,
+                payment._id,
+                'locked',
+                'pending'
+              )
 
-          if (!wasLocked) {
-            logger.info(
-              `Skipping payment ${payment._id} (already locked or processed)`
-            )
-            return null
-          }
+              const wasLocked =
+                lockResult &&
+                (lockResult.nModified === 1 ||
+                  lockResult.modifiedCount === 1 ||
+                  lockResult.n === 1)
 
-          let paymentHubData
-          if (grant.sourceSystem === 'FPTT') {
-            const identifiers = { sbi, frn, claimId }
-            paymentHubData = transformFpttPaymentDataToPaymentHubFormat(
-              identifiers,
-              grant,
-              payment
-            )
-          } else {
-            logger.error(
-              `Unsupported grant sourceSystem ${grant.sourceSystem} for payment ${payment._id}`
-            )
-            await updatePaymentStatus(docId, payment._id, 'failed')
-            return null
-          }
+              if (!wasLocked) {
+                logger.info(
+                  `Skipping payment ${payment._id} (already locked or processed)`
+                )
+                return null
+              }
 
-          try {
-            const res = await sendPaymentHubRequest(server, paymentHubData)
-            await updatePaymentStatus(docId, payment._id, 'submitted')
-            return res
-          } catch (err) {
-            logger.error(
-              err,
-              `${grafanaLogMessages.error.sendPaymentHubRequest} for record ${docId}`
-            )
-            await updatePaymentStatus(docId, payment._id, 'failed')
-            return serializeError(err)
-          }
-        })
-      })
-    })
+              let paymentHubData
+              if (grant.sourceSystem === 'FPTT') {
+                const identifiers = { sbi, frn, claimId }
+                paymentHubData = transformFpttPaymentDataToPaymentHubFormat(
+                  identifiers,
+                  grant,
+                  payment
+                )
+              } else {
+                logger.error(
+                  `Unsupported grant sourceSystem ${grant.sourceSystem} for payment ${payment._id}`
+                )
+                await updatePaymentStatus(docId, payment._id, 'failed')
+                return null
+              }
+
+              try {
+                const res = await sendPaymentHubRequest(server, paymentHubData)
+                await updatePaymentStatus(docId, payment._id, 'submitted')
+                return res
+              } catch (err) {
+                logger.error(
+                  err,
+                  `${grafanaLogMessages.error.sendPaymentHubRequest} for record ${docId}`
+                )
+                await updatePaymentStatus(docId, payment._id, 'failed')
+                return serializeError(err)
+              }
+            })()
+          )
+        }
+      }
+    }
 
     const results = await Promise.all(actions)
     return results
